ade998bbd098445e2794ae3de25868bc97f1d396
Young Lee <ncisoft@hotmail.com>
2018-05-22 20:27:44 +0800

    add Makefile

--- fork-cost/fork-cost.c
+++ fork-cost/fork-cost.c
@@ -1,3 +1,7 @@
+#define _POSIX_C_SOURCE 1
+#define _DEFAULT_SOURCE
+
+#include <unistd.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -5,8 +9,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
 #include <signal.h>
 
 union
@@ -15,13 +22,19 @@ union
   u_int64_t __uints[16384/sizeof(u_int64_t)];
 } ux;
 
-u_int64_t start_t;
-u_int64_t get_us()
+suseconds_t _tv_usec;    /* microseconds */
+u_int32_t start_t;
+u_int32_t get_us()
 {
-  clock_t t = clock();
+  struct timeval tv;
+  gettimeofday(&tv,NULL);
+  if (!_tv_usec)
+    _tv_usec = tv.tv_usec;
+#ifdef XXX
   double dt = (double) t / CLOCKS_PER_SEC ;	
   double usec = dt * 1000*1000;
-  return (u_int64_t)usec;
+#endif
+  return tv.tv_usec - _tv_usec;
 }
 
 void init()
@@ -44,28 +57,30 @@ void _xmalloc32M()
 
 void child_trap(int sig) 
 {
-  printf("%lu child_trap\n", start_t, get_us());
+  printf("%u child_trap\n", get_us());
 }
 
 void parent_trap(int sig) 
 {
-  printf("%lu parent_trap, child is alive\n", start_t, get_us());
+  printf("%u parent_trap, child is alive\n", get_us());
   sleep(0);
 }
 
 int main()
 {
   init();
-  printf("%lu init done, CLOCKS_PER_SEC=%lu\n", get_us(), CLOCKS_PER_SEC);
-  for (int i=0; i < 2; i++) //800 MB
+  printf("%u init done, CLOCKS_PER_SEC=%u\n", get_us(), (u_int32_t)CLOCKS_PER_SEC);
+  for (int i=0; i < 25; i++) //800 MB
     _xmalloc32M();
-  printf("%lu malloc 800-MB done\n", start_t, get_us());
+  usleep(100);
+  printf("%u malloc 800-MB done\n\n", get_us());
     {
       pid_t parent_pid, xpid;
       signal(SIGCHLD,SIG_IGN);
       signal(SIGUSR1, parent_trap);
 
       parent_pid = getpid();
+      printf("%u ready to fork\n", get_us());
       xpid = fork();
       if (xpid < 0)
         {
@@ -74,17 +89,18 @@ int main()
         }
       else if (xpid == 0)
         {
-          printf("\t %lu child is running\n", get_us());
+          printf("\t %u child is running\n", get_us());
           kill(parent_pid, SIGUSR1);
-          printf("\t %lu child is going to die\n", get_us());
+          printf("\t %u child is going to die\n", get_us());
           // child();
         }
       else
         {
           //parent, xpid is pid of child process
-          printf("%lu parent is running\n", start_t, get_us());
-          sleep(10);
-          printf("%lu parent is going to die\n", get_us());
+          printf("%u parent is running\n", get_us());
+          usleep(100);
+          printf("%u parent is going to die\n", get_us());
+          usleep(100);
         }
     }
   return 0;
