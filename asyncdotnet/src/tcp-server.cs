d1d46f8332102a827923b00b36e300d29958bf4f
Young Lee <ncisoft@hotmail.com>
2018-08-31 18:19:57 +0800

    feat: add worker thread pool support

--- asyncdotnet/src/tcp-server.cs
+++ asyncdotnet/src/tcp-server.cs
@@ -8,6 +8,8 @@ using System.Threading;
 using System.Net;
 using System.Net.Sockets;
 using  System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace csetcd
 {
@@ -21,30 +23,121 @@ namespace csetcd
       IWorker _worker = null; 
       private CancellationToken _ct = _getCancellationToken();
       private static NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+      private BlockingCollection<TcpClient>[] _mClientQueueList = new BlockingCollection<TcpClient>[ calculateWorkerThreadNum() ];
+      private BlockingCollection<TcpClient> _mSpareClientQueue = new BlockingCollection<TcpClient>();
+      private AsyncCallback mAcceptCallback;
 #endregion
 
-      void reusePort()
-      {
-	      _listener.Server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+      static int calculateWorkerThreadNum()
+        {
+          int num = Math.Max(2, Environment.ProcessorCount) - 2; // leave 1 io thread, 1 benchmark client process
+          num = Math.Max(2, num);
+          Console.WriteLine("worker thread count={0}", num);
 
+          return num;
+        }
+      void reusePort()
+        {
+          _listener.Server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+
+        }
+
+      public void initThreadWorker()
+        {
+          for (int i = 0; i < _mClientQueueList.Length; i++)
+            {
+               var  _mQueue = new BlockingCollection<TcpClient>();
+              _mClientQueueList[i] = _mQueue;
+              var _mThread = new Thread( () => { ThreadWorkerMain( i, this, _mQueue ); });
+              _mThread.IsBackground = true;
+
+              _mThread.Start();  
+              Thread.Sleep(1);
+            }
+
+          {
+            var _thread = new Thread(monitorGCMain);
+            _thread.IsBackground = true;
+            _thread.Start(); 
+            Thread.Sleep(1);
+          }
+
+              {
+                // fork spare thread
+                var _mQueue = _mSpareClientQueue;
+                var _thread = new Thread( () => { ThreadWorkerMain( 99, this, _mQueue ); });
+                _thread.IsBackground = true;
+                _thread.Start();
+                Thread.Sleep(1);
+              }
+        }
+
+      public void monitorGCMain()
+      {
+        Stopwatch sw = Stopwatch.StartNew();
+        int count = 0;
+        Console.WriteLine("starting GC monitor ...");
+        while (true)
+        {
+          int timeout = 20;
+          int sleep = 10; 
+          long start = sw.ElapsedMilliseconds;
+          Thread.Sleep(sleep);
+          long now = sw.ElapsedMilliseconds;
+          if ((now - start) >= (timeout + sleep))
+          {
+            Console.WriteLine( "  GC timeout[{0}]: {1} ms", count++, now-start-sleep );
+          }
+        }
+    }
+
+      [ThreadStatic]
+      private int mMaxQueueCount = 0;
+      public int getMaxQueueCount()
+      {
+        return mMaxQueueCount;
       }
+      
+      public void ThreadWorkerMain(int _mpId, TcpServer _mpTcpServer, BlockingCollection<TcpClient> _mpQueue)
+        {
+          Console.WriteLine("fork [{0}] thread, Id={1}", _mpId, Thread.CurrentThread.ManagedThreadId);
+          int _count = 0;
+          int iConnection = 0;
+          while (true)
+            {
+              IWorker worker = _worker.newInstance();
+              TcpClient client = _mpQueue.Take();
+              if (client == null)
+                throw new InvalidDataException("got null data from _mpQueue");
+              if (_mpQueue.Count >= _count + 5)
+              {
+                _count = _mpQueue.Count;
+                Console.WriteLine("tid=[{0}] max_queue={1}, iConnection={2}", Thread.CurrentThread.ManagedThreadId, _count, iConnection);
+              }
+             //   throw new InvalidDataException("_mpQueue.Count >= 10");
+          //Console.WriteLine("  threadId={0}", Thread.CurrentThread.ManagedThreadId);
+              worker.process(_mpTcpServer, client, _mpTcpServer._ct);
+              iConnection++;
+            }
+
+        }
       public void start(IWorker worker, string host, int port)
         {
           try
             {
               _worker = worker;
               _ct = _getCancellationToken();
+              initThreadWorker();
               IPAddress serverAddr = IPAddress.Parse(host);
               _listener = new TcpListener(serverAddr, port);
-              _listener.Start();
+              _listener.Start(128);
               _logger.Info("BeginAcceptTcpClient {0}:{1}", host, port);
-              _listener.BeginAcceptTcpClient(
-                                            new AsyncCallback(doAcceptListner), 
-                                            _listener);
+              mAcceptCallback = new AsyncCallback(doAcceptListner);
+              _listener.BeginAcceptTcpClient( mAcceptCallback, _listener );
               _logger.Info("BeginAcceptTcpClient...");
               Console.WriteLine("To quit, press eny key.");
               myReadKey();
-	      stop();
+              stop();
             }
           catch (Exception ex)
             {
@@ -64,7 +157,7 @@ namespace csetcd
           while (!_ct.IsCancellationRequested)
             {
               bool read = task.Wait(1000);
-	      if (read) _logger.Info("recv key");
+              if (read) _logger.Info("recv key");
               if (read) return task.Result;
             }
           return null;
@@ -77,21 +170,31 @@ namespace csetcd
           //Waits a little, to guarantee that all operation receive information about cancellation.
           Thread.Sleep(100);
           _listener.Stop();
-	  _logger.Info("stop()..");
-
+          _logger.Info("stop()..");
         }
 
+       static int _mSpareConnectionCount = 0;
       public void doAcceptListner( IAsyncResult ar )
         {
           if(_ct.IsCancellationRequested)
             return;
           TcpListener listener = (TcpListener) ar.AsyncState;
           TcpClient client = listener.EndAcceptTcpClient(ar);
-          _logger.Info("incoming client");
+          int id = client.Client.RemoteEndPoint.GetHashCode() % _mClientQueueList.Length;
+          if (id < 0) id = -id;
+          //Console.WriteLine("  id={0}", id);
           //Starts waiting for the next request.
-          listener.BeginAcceptTcpClient(doAcceptListner, listener);
-          IWorker worker = _worker.newInstance();
-          worker.process(this, client, _ct);
+          listener.BeginAcceptTcpClient(mAcceptCallback, listener);
+          {
+            var _queue = _mClientQueueList[id];
+            if (_queue.Count >= 5)
+            {
+              _queue = _mSpareClientQueue;
+               Console.WriteLine(" move connection to spare worker thread: c-{0} -> {1} from [{2}] worker", ++_mSpareConnectionCount, _queue.Count, id);
+            }
+            _queue.Add(client);
+          }
+          //_logger.Info("incoming client");
         }
     }
 
--- asyncdotnet/src/worker-redis-server.cs
+++ asyncdotnet/src/worker-redis-server.cs
@@ -12,193 +12,205 @@ using System.IO;
 
 namespace csetcd
 {
-  using static Utils;
+	using static Utils;
 
-  public class RedisWorker : IWorker
-  {
-    private static NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+	public class RedisWorker : IWorker
+	{
+		private static NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
 #region Fields.
-    TcpServer _tcpServer = null;
-    TcpClient _client;
-    NetworkStream _stream = null;
-    byte[] _bs = null;
-    CancellationToken _ct;
-    static readonly byte[] _bsOut = System.Text.Encoding.Default.GetBytes("+PONG\r\n");
-    static readonly byte[] _bsExit = System.Text.Encoding.Default.GetBytes("+OK exit\r\n");
+		TcpServer _tcpServer = null;
+		TcpClient _client;
+		NetworkStream _stream = null;
+		byte[] _bs = null;
+		CancellationToken _ct;
+		static readonly byte[] _bsOut = System.Text.Encoding.Default.GetBytes("+PONG\r\n");
+		static readonly byte[] _bsExit = System.Text.Encoding.Default.GetBytes("+OK exit\r\n");
 #endregion
-    ConcurrentDictionary<int, bool> _mThreadSet = new ConcurrentDictionary<int, bool>();
+		ConcurrentDictionary<int, bool> _mThreadSet = new ConcurrentDictionary<int, bool>();
 
-    public RedisWorker()
-      {
-      }
+		public RedisWorker()
+		{
+		}
 
 #region IWorker implemetation
-    IWorker IWorker.newInstance()
-      {
-        return  new RedisWorker();
-      }
-    void IWorker.process(TcpServer tcpServer, TcpClient client, CancellationToken ct)
-      {
-        try {
-            _tcpServer = tcpServer;
-            _client = client;
-            _stream = _client.GetStream();
-            _ct = ct;
-            _stream.ReadTimeout = 1000;
-            _bs = new byte[BUF_SIZE];
+		IWorker IWorker.newInstance()
+		{
+			return  new RedisWorker();
+		}
+		
+		
+		void IWorker.process(TcpServer tcpServer, TcpClient client, CancellationToken ct)
+		{
+			try {
+				_tcpServer = tcpServer;
+				_client = client;
+				_stream = _client.GetStream();
+				_ct = ct;
+				_stream.ReadTimeout = 1000;
+				_bs = _bs ?? new byte[BUF_SIZE];
 
-            _process();
-        }
-        catch (Exception ex)
-          {
-            _logger.Debug(ex.Message);
-          }
-      }
-    string trim(byte[] bs, int length)
-      {
-        int len = length;
-        while (len > 0 && bs[len-1] == '\n')
-          {
-            len--;
-            if (len > 0 && bs[len-1] == '\r')
-              len--;
-          }
-        _logger.Debug("before={0}, after={1}", length, len);
-        return System.Text.Encoding.Default.GetString(bs, 0, len);
-      }
+				_process();
+			}
+			catch (Exception ex)
+			{
+				_logger.Debug(ex.Message);
+			}
+		}
+		string trim(byte[] bs, int length)
+		{
+			int len = length;
+			while (len > 0 && bs[len-1] == '\n')
+			{
+				len--;
+				if (len > 0 && bs[len-1] == '\r')
+					len--;
+			}
+			_logger.Debug("before={0}, after={1}", length, len);
+			return System.Text.Encoding.Default.GetString(bs, 0, len);
+		}
 
-    string parse_redis_protocol(byte[] bs, int length)
-      {
-        int offset = 0;
-        if (bs[0] == '*' && bs[1] == '1')
-          {
-            offset += 4;
-            if (bs[offset] == '$')
-              {
-                int  nbytes = bs[offset+1] - '0';
-                offset += 4;
-                return System.Text.Encoding.Default.GetString(bs, offset, nbytes);
-              }
-          }
-        else
-          {
-            return trim(bs, length);
-          }
-        return "unknown";
-      }
-    void dump_bs(string msg, byte[] ba, int length)
-      {
-        byte[] bs = new byte[length];
-        Array.Copy(ba, 0, bs, 0, length);
-        _logger.Debug("{0}, length={1}\n", msg, length);
-        //Console.WriteLine( Hex.Dump(bs) );
-      }
+		string parse_redis_protocol(byte[] bs, int length)
+		{
+			int offset = 0;
+			if (bs[0] == '*' && bs[1] == '1')
+			{
+				offset += 4;
+				if (bs[offset] == '$')
+				{
+					int  nbytes = bs[offset+1] - '0';
+					offset += 4;
+					return System.Text.Encoding.Default.GetString(bs, offset, nbytes);
+				}
+			}
+			else
+			{
+				return trim(bs, length);
+			}
+			return "unknown";
+		}
+		void dump_bs(string msg, byte[] ba, int length)
+		{
+			byte[] bs = new byte[length];
+			Array.Copy(ba, 0, bs, 0, length);
+			_logger.Debug("{0}, length={1}\n", msg, length);
+			//Console.WriteLine( Hex.Dump(bs) );
+		}
 
-    async Task meth_ping()
-      {
-        _logger.Debug("received redis ping command");
-        await _stream.WriteAsync(_bsOut, 0, _bsOut.Length);
-      }
+		async Task meth_ping()
+		{
+			_logger.Debug("received redis ping command");
+			await _stream.WriteAsync(_bsOut, 0, _bsOut.Length);
+		}
 
-    void close()
-      {
-        _stream.Close();
-        _client.Close();
-        _bs = null;
-        _stream = null;
-        _client = null;
-      }
-    async Task meth_exit()
-      {
-        await _stream.WriteAsync(_bsExit, 0, _bsOut.Length);
-        _tcpServer.stop();
-        _logger.Fatal("will shutdown");
-        close();
-      }
+		void close()
+		{
+			if (_client == null)
+				return;
+			_stream.Close();
+			_client.Close();
+			_bs = null;
+			_stream = null;
+			_client = null;
+		}
+		async Task meth_exit()
+		{
+			await _stream.WriteAsync(_bsExit, 0, _bsOut.Length);
+			_tcpServer.stop();
+			_logger.Fatal("will shutdown");
+			close();
+		}
 
-    async Task meth_gcstatus()
-      {
-        string msg = string_format("+OK gc.status \n    ",
-                                   "maxGeneration={0}, ", GC.MaxGeneration,
-                                   "totalMemory={1:N0}\n    ", GC.GetTotalMemory(false),
-                                   "collectionCount[0]={2}\n    ", GC.CollectionCount(0),
-                                   "collectionCount[1]={3}\n    ", GC.CollectionCount(1),
-                                   "collectionCount[2]={4}\n    ", GC.CollectionCount(2),
-                                   "max_thread_count={5}\n\n",   _mThreadSet.Count
-        );
-        // reset _mThreadSet
-        _mThreadSet = new ConcurrentDictionary<int, bool>();
-        byte[] bs = System.Text.Encoding.Default.GetBytes(msg);
-        await _stream.WriteAsync(bs, 0, bs.Length);
-        close();
-      }
-    async Task meth_gccollect()
-      {
-        string msg = string_format("+OK gc.status before\n    ",
-                                   "maxGeneration={0}, ", GC.MaxGeneration,
-                                   "totalMemory={1:N0}\n    ", GC.GetTotalMemory(false),
-                                   "collectionCount[0]={2} \n    ", GC.CollectionCount(0),
-                                   "collectionCount[1]={3} \n   ", GC.CollectionCount(1),
-                                   "collectionCount[2]={4}\n\n ", GC.CollectionCount(2)
-        );
+		int getCurrentThreadId()
+		{
+			Thread _thread = Thread.CurrentThread;
+			return (_thread != null) ? _thread.ManagedThreadId : -1;
+		}
+		async Task meth_gcstatus()
+		{
+			string msg = string_format("+OK gc.status \n    ",
+					"maxGeneration={0}, ", GC.MaxGeneration,
+					"totalMemory={1:N0}    \n    ", GC.GetTotalMemory(false),
+					"collectionCount[0]={2}\n    ", GC.CollectionCount(0),
+					"collectionCount[1]={3}\n    ", GC.CollectionCount(1),
+					"collectionCount[2]={4}\n    ", GC.CollectionCount(2),
+					"processor_count={5}   \n    ", Environment.ProcessorCount,
+					"threadId={6}          \n    ", getCurrentThreadId(),
+					"max_thread_count={7}  \n    ",   _mThreadSet.Count,
+					"max_queue_count={8}\n\n",   _tcpServer.getMaxQueueCount()
+				
+					);
+			// reset _mThreadSet
+			_mThreadSet = new ConcurrentDictionary<int, bool>();
+			byte[] bs = System.Text.Encoding.Default.GetBytes(msg);
+			await _stream.WriteAsync(bs, 0, bs.Length);
+			close();
+		}
+		async Task meth_gccollect()
+		{
+			string msg = string_format("+OK gc.status before\n    ",
+					"maxGeneration={0}, ", GC.MaxGeneration,
+					"totalMemory={1:N0}\n    ", GC.GetTotalMemory(false),
+					"collectionCount[0]={2} \n    ", GC.CollectionCount(0),
+					"collectionCount[1]={3} \n   ", GC.CollectionCount(1),
+					"collectionCount[2]={4}\n\n ", GC.CollectionCount(2)
+					);
 
-        byte[] bs = System.Text.Encoding.Default.GetBytes(msg);
-        await _stream.WriteAsync(bs, 0, bs.Length);
-        GC.Collect();
-        GC.WaitForPendingFinalizers();
-        GC.Collect();
-        await meth_gcstatus();
-      }
+			byte[] bs = System.Text.Encoding.Default.GetBytes(msg);
+			await _stream.WriteAsync(bs, 0, bs.Length);
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+			GC.Collect();
+			await meth_gcstatus();
+		}
 
-    async void _process()
-      {
-        while (!_ct.IsCancellationRequested && _stream != null)
-          {
-            try
-              {
-                int nRead = await _stream.ReadAsync(_bs, 0, _bs.Length);
-                if (nRead > 0)
-                  {
-                    dump_bs("dump incoming msg", _bs, nRead);
-                    string str = parse_redis_protocol(_bs, nRead);
-                    byte[] bs2 = System.Text.Encoding.Default.GetBytes(str);
-                    dump_bs("dump incoming msg", bs2, bs2.Length);
-                    _logger.Warn("incoming cmd={0} , Length={1},{2}",  str, nRead, str.Length);
-                    _mThreadSet.TryAdd(Thread.GetCurrentProcessorId(), true);
-                    switch (str.ToLower())
-                      {
-                      case "ping":
-                        await meth_ping();
-                        break;
-                      case "gcstatus":
-                        await meth_gcstatus();
-                        break;
-                      case "gccollect":
-                        await meth_gccollect();
-                        break;
-                      case "exit":
-                        await meth_exit();
-                        break;
-                      default:
-                        Console.WriteLine("illegal cmd {0}, Length={1}", str, nRead);
-                        break;
-                      }
-                  }
-                else
-                  {
-                    if (!isSocketConnected(_client.Client))
-                      _client.Client.Disconnect(true);
-                    break;
-                  }
-              }
-            catch (Exception ex)
-              {
-                _logger.Info("exception is {0}:{1}", ex.GetType(), ex.Message);
-                _client.Close();
-                break;
-              }
-          }
-      }
+		async void _process()
+		{
+			while (!_ct.IsCancellationRequested && _stream != null)
+			{
+				try
+				{
+					int nRead = await _stream.ReadAsync(_bs, 0, _bs.Length);
+					if (nRead > 0)
+					{
+						dump_bs("dump incoming msg", _bs, nRead);
+						string str = parse_redis_protocol(_bs, nRead);
+						byte[] bs2 = System.Text.Encoding.Default.GetBytes(str);
+						dump_bs("dump incoming msg", bs2, bs2.Length);
+						_logger.Warn("incoming cmd={0} , Length={1},{2}",  str, nRead, str.Length);
+						_mThreadSet.TryAdd(Thread.CurrentThread.ManagedThreadId, true);
+						switch (str.ToLower())
+						{
+							case "ping":
+								await meth_ping();
+								break;
+							case "gcstatus":
+								await meth_gcstatus();
+								break;
+							case "gccollect":
+								await meth_gccollect();
+								break;
+							case "exit":
+								await meth_exit();
+								break;
+							default:
+								Console.WriteLine("illegal cmd {0}, Length={1}", str, nRead);
+								break;
+						}
+					}
+					if (!isSocketConnected(_client.Client))
+					{
+						_client.Client.Disconnect(true);
+						break;
+					}
+				}
+				catch (Exception ex)
+				{
+					_logger.Info("exception is {0}:{1}", ex.GetType(), ex.Message);
+					this.close();
+					break;
+				}
+			}
+		}
 #endregion
-  }
+	}
 }
